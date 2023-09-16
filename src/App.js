a83f4c630015b8e12cfce1ed0fdb4531aca8fb72
Diana Nnaemeka <104990762+dev-diana@users.noreply.github.com>
2023-09-16 22:33:57 +0100

    web src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import MovieSection from './components/MovieSection.js';
+
+function App() {
+  return (
+    <div className="font-main">
+      <Header />
+      <MovieSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
